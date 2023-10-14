4e48ef6eab5fada6d10b9fbfa18d1f0fcd576af7
Peter Ma <peterma1999@gmail.com>
2023-10-14 17:08:56 -0400

    Implmented PUT reservation functionality for updates

--- src/main/java/com/example/bugyourspot/reservation/ReservationController.java
+++ src/main/java/com/example/bugyourspot/reservation/ReservationController.java
@@ -1,7 +1,9 @@
 package com.example.bugyourspot.reservation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -27,5 +29,14 @@ public class ReservationController {
     @DeleteMapping(path = "{id}")
     public void cancelReservation(@PathVariable("id") Long id) {
         reservationService.deleteReservation(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateReservation(
+            @PathVariable("id") Long id,
+            @RequestParam(required = false) LocalDateTime startTime,
+            @RequestParam(required = false) LocalDateTime endTime
+    ) {
+        reservationService.updateReservation(id, startTime, endTime);
     }
 }
--- src/main/java/com/example/bugyourspot/reservation/ReservationService.java
+++ src/main/java/com/example/bugyourspot/reservation/ReservationService.java
@@ -1,11 +1,13 @@
 package com.example.bugyourspot.reservation;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,21 @@ public class ReservationService {
             throw new IllegalStateException("reservation with id " + id + " does not exist");
         }
         reservationRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateReservation(Long id, LocalDateTime startTime, LocalDateTime endTime) {
+        Reservation reservation = reservationRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("reservation with id " + id + " does not exist"));
+
+        if (startTime != null && !Objects.equals(reservation.getStartTime(), startTime)) {
+            // TODO: limit number of reservations per certain time slot
+            reservation.setStartTime(startTime);
+        }
+
+        if (endTime != null && !Objects.equals(reservation.getEndTime(), endTime)) {
+            // TODO: Check if start time < end time
+            reservation.setEndTime(endTime);
+        }
     }
 }
